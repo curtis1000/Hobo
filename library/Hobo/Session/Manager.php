6279b217e07cb8dff42463a271508e8cf3029ef0
Curtis Branum <curtis.branum@gmail.com>
2012-07-01 22:40:30 -0500

    added user api

---
+++ library/Hobo/Session/Manager.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Used by Hobo_User
+ */
+class Hobo_Session_Manager
+{
+    protected $_session;
+
+    public function get()
+    {
+        if (empty($this->_session)) {
+            $this->_session = new Zend_Session_Namespace('hobo_' . APPLICATION_ENV);
+        }
+        return $this->_session;
+    }
+
+    public function destroy()
+    {
+        $session = $this->getSession();
+        $session = new stdClass();
+    }
+}
---
+++ library/Hobo/User.php
@@ -0,0 +1,36 @@
+<?php
+
+class Hobo_User
+{
+    protected $_session;
+
+    protected function initSession()
+    {
+        if (empty($this->_session)) {
+            $hoboSessionManager = new Hobo_Session_Manager();
+            $this->_session = $hoboSessionManager->get();
+        }
+    }
+
+    // called from external code, whichever user system the site implements will
+    // need to call this to enable the cms for admin users
+    public function logIn($username)
+    {
+        $this->initSession();
+        $this->_session->username = $username;
+    }
+
+    public function isLoggedIn()
+    {
+        $this->initSession();
+        return (! empty($this->_session->username));
+    }
+
+    public function getUsername()
+    {
+        $this->initSession();
+        if ($this->isLoggedIn()) {
+            return $this->_session->username;
+        }
+    }
+}
